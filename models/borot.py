c4f5c9424417cfc932d4086527dfc4199c0e7ee1
Noah Scharrenberg <nscharrenberg@hotmail.com>
2024-04-11 17:47:08 +0200

    Add Map Generation + Sensors to Continuous Space Game

--- models/borot.py
+++ models/borot.py
@@ -1,5 +1,7 @@
 from typing import Tuple
 
+import numpy as np
+
 BOROT_SIZE = 10
 
 class Borot:
@@ -11,15 +13,53 @@ class Borot:
         self.velocity: Tuple[float, float] = (0, 0)  # Left and Right Wheel
         self.rotation = 0
         self.icc: Tuple[float, float] = (0, 0)  # Instantaneous Center of Curvature
-
-    def move(self, direction) -> Tuple[int, int]:
-        new_pos = self.position + direction
-        # Check if the new position is within the maze and not a wall
-        if 0 <= new_pos.x < self.world.width and 0 <= new_pos.y < self.world.height:
-            if self.world.obstacles[int(new_pos.y)][int(new_pos.x)] == 0:
-                self.position = new_pos
-
-        return self.get_position()
+        self.Vl = 0  # left velocity
+        self.Vr = 0  # right velocity
+        self.theta = 0
+        self.speed = 5
 
     def get_position(self) -> Tuple[int, int]:
         return self.position
+
+    def moving_keys(self, keys, dt, screen, player):
+        self.screen = screen
+        self.x = self.position[0]  # starting x
+        self.y = self.position[1]  # starting y
+        self.radius = player
+
+        # keys = [w, s, o, l, x, t, g]
+        if keys[0] == 1 or keys[5] == 1:
+            self.Vr += self.speed
+            print(self.Vr)
+        elif keys[1] == 1 or keys[6] == 1:
+            self.Vr -= self.speed
+        elif keys[2] == 1 or keys[5] == 1:
+            self.Vl += self.speed
+        elif keys[3] == 1 or keys[6] == 1:
+            self.Vl -= self.speed
+        elif keys[4] == 1:
+            self.Vl = 0
+            self.Vr = 0
+
+        # If it's moving
+        if self.Vr != 0 or self.Vl != 0:
+
+            # Make sure not to get a division by zero when velocities are the same
+            if self.Vr == self.Vl:
+                next_x = self.x + ((self.Vl + self.Vr) / 2) * np.cos(self.theta) * dt
+                next_y = self.y - ((self.Vl + self.Vr) / 2) * np.sin(self.theta) * dt
+                new_theta = self.theta + (self.Vr - self.Vl) / (2 * self.radius) * dt
+                self.position = (next_x, next_y)
+            else:
+                R = self.radius * (self.Vl + self.Vr) / (self.Vr - self.Vl)
+                w = (self.Vr - self.Vl) / (self.radius * 2)
+                # Compute ICC
+                ICC = [self.x - R * np.sin(-self.theta), self.y + R * np.cos(self.theta)]
+                result = np.transpose(np.matmul(
+                    np.array([[np.cos(w * dt), -np.sin(w * dt), 0],
+                              [np.sin(w * dt), np.cos(w * dt), 0],
+                              [0, 0, 1]]),
+                    np.transpose(np.array([self.x - ICC[0], self.y - ICC[1], self.theta]))) + np.array(
+                    [ICC[0], ICC[1], w * dt])).transpose()
+                next_x, next_y, new_theta = result[0], result[1], result[2]  # info for updateding the sensors
+                self.position = (next_x, next_y)
--- utils/sensors.py
+++ utils/sensors.py
@@ -1,32 +1,31 @@
 import math
 import pygame
 
+
 class Sensors:
     def __init__(self, player_pos, screen):
         self.player_pos = player_pos
         self.screen = screen
-        #self.obstacles = obstacles  # Assume this is a list of obstacle objects
+        # self.obstacles = obstacles  # Assume this is a list of obstacle objects
         self.STEP_ANGLE = (math.pi * 2) / 12
         self.SENSORS_FONT = pygame.font.SysFont("comicsans", 15)
         self.sensor_length = 100  # Maximum sensor ray length
 
-        
     def test_cast_rays(self):
         temp_angle = 0
         for i in range(12):
-            end_x = self.player_pos.x + math.cos(temp_angle) * self.sensor_length
-            end_y = self.player_pos.y + math.sin(temp_angle) * self.sensor_length
-            pygame.draw.line(self.screen, (255, 255, 255), (self.player_pos.x, self.player_pos.y), (end_x, end_y), 1)
+            end_x = self.player_pos[0] + math.cos(temp_angle) * self.sensor_length
+            end_y = self.player_pos[1] + math.sin(temp_angle) * self.sensor_length
+            pygame.draw.line(self.screen, (255, 255, 255), (self.player_pos[0], self.player_pos[1]), (end_x, end_y), 1)
             temp_angle += self.STEP_ANGLE
-    
 
     def cast_rays(self):
         self.all_sensors = []
         temp_angle = 0
         for i in range(12):
             self.all_sensors.append({
-                "x": self.player_pos.x,
-                "y": self.player_pos.y,
+                "x": self.player_pos[0],
+                "y": self.player_pos[1],
                 "angle": temp_angle,
                 "index": i
             })
@@ -43,7 +42,8 @@ class Sensors:
                 for obstacle in self.obstacles:
                     clipped_line = obstacle.clipline(ray)
                     if clipped_line:
-                        distance = int(math.sqrt((clipped_line[0][1] - sensor['y'])**2 + (clipped_line[0][0] - sensor['x'])**2)) - 32
+                        distance = int(math.sqrt(
+                            (clipped_line[0][1] - sensor['y']) ** 2 + (clipped_line[0][0] - sensor['x']) ** 2)) - 32
                         if distance < sensor['distance']:
                             sensor['distance'] = distance
                             sensor['collision_point'] = clipped_line[0]
